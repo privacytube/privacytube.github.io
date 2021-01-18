9ce1c3933898d8cc29e964c4bd1824a15d357f5b
v <>
2021-01-18 15:02:57 -0500

    Update stream.js

--- js/stream.js
+++ js/stream.js
@@ -1,18 +1,24 @@
 importScripts("ponyfill.es6.js");
 
-
+const text = new TextDecoder();
 self.onmessage = async function (e) {
 
 
 
 let response = await fetch(e.data);
-const reader = response.body.pipeThrough(new TextDecoderStream()).getReader();
+
+const response = await fetch(url);
+const reader = response.body.getReader();
+
 while (true) {
   const { value, done } = await reader.read();
   if (done) break;
-  console.log('Received', value);
+  console.log('Received', text.decode(value));
 }
 
 console.log('Response fully received');
+
+
+
 
 }
