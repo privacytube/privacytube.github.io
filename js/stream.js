83579b04e5d1abf2bde010a483683a04b2d6413d
v <>
2021-01-18 16:28:01 -0500

    Update stream.js

--- js/stream.js
+++ js/stream.js
@@ -12,7 +12,7 @@ for (i in data){
 
   var u = 'https://invidiou.site' + data[i].authorUrl
   var v = 'https://invidiou.site/embed/' + data[i].videoId
-  const display =  "<a href='"+ v +"'><img src='" + data[i].videoThumbnails[4].url + "'></a><br><a href='"+  v  + "'>" + data[i].title + "</a><br><a href='" + u +"'>" + data[i].author + "</a>"
+  const display =  "<a href='"+ v +"'><img src='" + data[i].videoThumbnails[2].url + "'></a><br><a href='"+  v  + "'>" + data[i].title + "</a><br><a href='" + u +"'>" + data[i].author + "</a>"
   self.postMessage(display);
 
 
