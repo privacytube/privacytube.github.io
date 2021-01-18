b584a4f750587130bfdf2f3e8cbd25afe651a8e5
v <>
2021-01-18 15:59:26 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 
 var stream = new Worker("js/stream.js")
-
 var but = document.getElementById('s');
 var inp = document.getElementById('input').value;
+var form = document.querySelector("form");
 
 
 but.onclick = clicked;
 
 function clicked() {
+
+form.style.display = "none";
 
 var url = "https://invidiou.site/api/v1/search?q=" + inp;
 stream.postMessage(url);
