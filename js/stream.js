58c7f09270656c2bd29ef292535bdf6ff862657f
v <>
2021-01-18 18:51:17 -0500

    Update stream.js

--- js/stream.js
+++ js/stream.js
@@ -1,4 +1,3 @@
-const text = new TextDecoder();
 self.onmessage = function (e) {
 
 
@@ -9,7 +8,7 @@ for (i in data){
 
   var u = 'https://invidiou.site' + data[i].authorUrl
   var v = 'https://invidiou.site/embed/' + data[i].videoId
-  const display =  "<a href='"+ v +"'><img alt='"+ data[i].title +"'src='" + data[i].videoThumbnails[3].url + "'></a><br><a href='"+  v  + "'>" + data[i].title + "</a><br><br><a href='" + u +"'>" + data[i].author + "</a>"
+  const display =  "<a href='"+ data[i].author +"'><img alt='"+ data[i].title +"'src='" + data[i].videoThumbnails[3].url + "'></a><br><a href='"+  v  + "'>" + data[i].title + "</a><br><br><a href='" + u +"'>" + data[i].author + "</a>"
   self.postMessage(display);
 
 
