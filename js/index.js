c2c58cf8b9ab41769cf089d103115c9e40157d49
v <>
2021-01-18 18:58:31 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,12 +2,13 @@
 var stream = new Worker("js/stream.js")
 var but = document.getElementById('s');
 var form = document.querySelector("form");
-
+var is = document.getElementById('first');
 
 but.onclick = clicked;
 
 function clicked(e) {
 e.preventDefault;
+is.innerHTML = "<span></span>";
 var inp = document.getElementById('input').value;
 if(inp!=="pewdiepie"&&inp!=="kalvin garrah"&&inp!=="kalvin"&&inp!=="garrah"){
 
@@ -20,7 +21,7 @@ stream.onmessage = function (e) {
 
   element.innerHTML = e.data;
 
-  document.getElementById('first').prepend(element);
+  document.getElementById('first').append(element);
 
 }
 
