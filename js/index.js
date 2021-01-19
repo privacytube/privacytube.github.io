df09e003fe5de1dc3fc8f7677869a227b36e169b
v <>
2021-01-18 21:23:45 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -33,3 +33,15 @@ form.style.display = "none";
 }
 
 }
+
+
+if ('serviceWorker' in navigator) {
+
+  navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+    console.log('Service worker registration succeeded:', registration);
+  },function(error) {
+    console.log('Service worker registration failed:', error);
+  });
+} else {
+  console.log('Service workers are not supported.');
+}
