4d767a693de05b5d270bf4c0c15f322d04288ec0
v <>
2021-01-18 20:24:02 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,17 @@
 
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    but.click();
+  }
+});
+
+
 var stream = new Worker("js/stream.js")
 var but = document.getElementById('s');
 var form = document.querySelector("form");
@@ -37,14 +50,3 @@ form.style.display = "none";
 
 
 var input = document.getElementById("input");
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    but.click();
-  }
-});
