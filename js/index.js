43806759db4cf6ffcbfb443c6be8e9a2f047fdde
v <>
2021-01-18 19:50:36 -0500

    click if enter is pressed

--- js/index.js
+++ js/index.js
@@ -34,3 +34,17 @@ form.style.display = "none";
 }
 
 }
+
+
+var input = document.getElementById("input");
+
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    but.click();
+  }
+});
