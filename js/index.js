1320357610b9da6e702babce6828da80e628b740
v <>
2021-01-18 20:25:41 -0500

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,17 +1,3 @@
-
-
-// Execute a function when the user releases a key on the keyboard
-input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    but.click();
-  }
-});
-
-
 var stream = new Worker("js/stream.js")
 var but = document.getElementById('s');
 var form = document.querySelector("form");
